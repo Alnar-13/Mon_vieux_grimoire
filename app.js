4a6c4d5f2dfbfd1cf65ab30807beddf0640a3109
Kayzer <max.mlv2210@gmail.com>
2024-06-27 16:52:56 +0200

    second commit

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue" });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTION');
+    next();
+});
+
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl: "",
+      price: 4900,
+      userId: "gsomihvgios", // Correction: 'usezId' to 'userId'
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl: "",
+      price: 2900,
+      userId: "gsomihvgios", // Correction: 'usezId' to 'userId'
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 module.exports = app;
