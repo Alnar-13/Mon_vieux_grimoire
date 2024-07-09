190734889914bdb9ed6bd34ad736789ba67ffad1
Kayzer <max.mlv2210@gmail.com>
2024-07-09 14:43:51 +0200

    modif controllers/user

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
   console.log("Received signup request");
@@ -57,37 +57,6 @@ exports.login = (req, res, next) => {
             token: jwt.sign(
               { userId: user._id },
               process.env.JWT_SECRET || "RANDOM_TOKEN_SECRET",
-              { expiresIn: "24h" }
-            ),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
-};
-
-
-exports.login = (req, res, next) => {
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).json({ error: "Email et mot de passe requis" });
-  }
-
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ message: "Paire identifiant/mot de passe incorrect" });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ message: "Paire identifiant/mot de passe incorrect" });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              process.env.JWT_SECRET || "RANDOM_TOKEN_SECRET", // Utilisation de la variable d'environnement
               { expiresIn: "24h" }
             ),
           });
