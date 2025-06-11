e5df4e1d94223c7b2a601f022d6a3c696bb9a70a
Alnar-13 <z.mennouja@gmail.com>
2025-06-11 19:57:39 +0100

    improvement 4

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,10 @@ require('dotenv').config({ path: '.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const bodyParser = require('express');
 const booksRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 
-mongoose.connect(
-  process.env.CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-)
+mongoose.connect(process.env.CONNECTION_STRING)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 app.use(express.json());
@@ -24,7 +17,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/api/books', booksRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static('images'));
